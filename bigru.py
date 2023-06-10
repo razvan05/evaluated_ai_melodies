8b37ba6e9c5e95d453745a18bce3909232448036
razvan05 <razvan_just4u@yahoo.com>
2023-06-10 14:11:25 +0300

    evaluation scores and source code

---
+++ bigru.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import GRU
+from tensorflow.keras.layers import Bidirectional
+from tensorflow.keras.layers import Input
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Embedding
+import sys
+
+def near_disonances_notes_model(config, notes_encoding_size, durations_encoding_size):
+
+    # for training
+    seq_len = config.sequence_length
+    encoder_input = Input(shape = (seq_len, durations_encoding_size))
+    
+    forward_encoder = GRU(config.hidden_values, return_state = True)
+    backward_encoder = GRU(config.hidden_values, return_state = True, go_backwards = True)
+    f_encoder_output, f_state_h = forward_encoder(encoder_input)
+    b_encoder_output, b_state_h = backward_encoder(encoder_input)
+
+    decoder_input = Input(shape = (seq_len, notes_encoding_size))
+    
+    forward_decoder = GRU(config.hidden_values, return_state = True)
+    backward_decoder = GRU(config.hidden_values, return_state = True, go_backwards = True)
+    
+    bigru_decoder = Bidirectional(forward_decoder, backward_layer = backward_decoder) 
+    bigru_decoder_output, f_h, b_h = bigru_decoder(decoder_input, initial_state = [f_state_h, b_state_h])
+
+    tf.print(bigru_decoder_output.shape, output_stream = sys.stdout)
+    decoder_dense =  Dense(1, activation = 'sigmoid')
+    decoder_output = decoder_dense(bigru_decoder_output)
+    tf.print(decoder_output.shape, output_stream = sys.stdout)
+    
+    model = Model([encoder_input, decoder_input], decoder_output)
+
+    # for prediction
+    encoder_output_states = [f_state_h, b_state_h]
+    encoder_model = Model(encoder_input, encoder_output_states)
+
+    f_decoder_state_input_h = Input(shape = (config.hidden_values,))
+    b_decoder_state_input_h = Input(shape = (config.hidden_values,))
+    decoder_input_states = [f_decoder_state_input_h, b_decoder_state_input_h]
+    
+    bigru_decoder_output, f_h, b_h = bigru_decoder(decoder_input, initial_state = decoder_input_states)
+    decoder_states = [f_h, b_h] 
+    
+    decoder_dense = decoder_dense(bigru_decoder_output)
+
+    decoder_model = Model([decoder_input] + decoder_input_states, [decoder_dense] + decoder_states)
+
+    opt = tf.keras.optimizers.Adam(learning_rate = config.learning_rate)
+    bce = tf.keras.losses.BinaryCrossentropy()
+    model.compile(loss = 'bce', optimizer = opt, metrics = ['accuracy'], run_eagerly = False)
+
+    return model, encoder_model, decoder_model
---
+++ generateEstheticsScores.py
@@ -0,0 +1,110 @@
+import os, sys
+from music21 import converter, instrument, note, chord, stream, midi, meter, interval
+import numpy as np
+
+import bigru
+
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import Model
+from tqdm import tqdm
+import gc
+
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2000)])
+    except RuntimeError as e:
+        print(e)
+class Config():
+    model = None
+    
+def generate():       
+    notes = []
+    durations = []
+    
+    notes_not_disonances = []
+    durations_not_disonances = []
+
+    #define model hyperparameters
+    config = Config()
+    config.model = 'bigru'
+    config.learning_rate = 0.0001
+    config.sequence_length = 20
+    config.hidden_values = 200
+    config.epochs = 1
+    config.batch_size = 32
+    
+    unique_notes = np.load('./models/unique_notes.npy', allow_pickle = True)
+    unique_dur = np.load('./models/unique_dur.npy', allow_pickle = True)
+
+    #integer encoding
+    note_to_int = dict((note, i) for i, note in enumerate(unique_notes))
+    dur_to_int = dict((dur, i) for i, dur in enumerate(unique_dur))
+    
+    encoded_notes = [note_to_int[note] for note in unique_notes]    #126 (18 scales * 7 notes)
+    encoded_dur = [dur_to_int[dur] for dur in unique_dur]           #5159
+    model, _, _ = bigru.near_disonances_notes_model(config, len(to_categorical(encoded_notes)), len(to_categorical(encoded_dur)))
+    model.load_weights('./models/bigru/my_checkpoint')
+    
+    
+    #read melody
+    midi_file = converter.parse('melody8.mid')  
+    
+    m = meter.TimeSignature('4/4')
+    s = stream.Score(id='mainScore')
+
+    voice = stream.Part(id = 'part' + str(1))
+    voice.append(m)
+    notes_to_parse = midi_file.flat.notes
+    
+    notes_seq = []
+    dur_seq = []
+    
+    good_disonances = 0
+    bad_disonances = 0
+    
+    for element in notes_to_parse: # for each note from the current melody
+        if isinstance(element, note.Note):
+            current_note = element.pitch
+            notes_seq.append(current_note)
+            
+            current_dur = element.duration.quarterLength
+            dur_seq.append(current_dur)
+            
+            s_len = config.sequence_length
+            
+            if len(notes_seq) == s_len:  # seq of 20 notes
+            
+                # we have a real dissonance
+                if interval.Interval(noteStart = notes_seq[s_len // 2 - 2], noteEnd = notes_seq[s_len // 2 - 1]).simpleName in ["m2", "M2", "TT", "m7", "M7"]: 
+                    
+                    #encode notes/dur for NN input
+                    encoded_notes = [note_to_int[str(note)] for note in notes_seq]    #126 (18 scales * 7 notes)
+                    encoded_dur = [dur_to_int.get(str(dur), 0) for dur in dur_seq]
+                    
+                    encoded_notes = to_categorical(encoded_notes, num_classes = len(note_to_int))   
+                    encoded_dur = to_categorical(encoded_dur, num_classes = len(dur_to_int))
+                    
+                    #predict if there is a disonance between note 9 and 10
+                    prediction = model.predict_on_batch([tf.expand_dims(encoded_dur, axis = 0), 
+                                                         tf.expand_dims(encoded_notes, axis = 0)])
+                    prediction = tf.squeeze(prediction, axis = 0)
+                
+                    if prediction[0] < 0.25: # it predicts that there is NO dissonance
+                        bad_disonances += 1
+                    else: # it predicts that there is a dissonance
+                        good_disonances += 1
+                        
+                notes_seq.pop(0)
+                dur_seq.pop(0)
+                
+    if good_disonances + bad_disonances == 0:
+        print("There are no dissonances")
+    else:
+        print("Good disonances:  " + str(good_disonances))
+        print("Bad disonances:  " + str(bad_disonances))
+        print("Aesthetic score = " + str((good_disonances * good_disonances) / ((good_disonances + 9 * bad_disonances) * (good_disonances + 9 * bad_disonances))))
+
+generate()
